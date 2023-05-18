7420ee2c0906cfe7856e70a304c74b168ea5c258
voolyvex <1joeknapp@gmail.com>
2023-05-18 15:54:51 -0500

    initial commit

--- DiceGameMethodLab/Program.cs
+++ DiceGameMethodLab/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-            //Do not change anything in this file!
 
             //Instantiating a Game object
             Game game = new Game();
